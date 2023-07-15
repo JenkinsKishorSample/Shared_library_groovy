c50100a052b42adee2ee695246142bdd48f46a8f
kishor <kishor.anilkumar@inspirisys.com>
2023-07-15 10:48:28 +0530

    shared library

---
+++ shared.groovy
@@ -0,0 +1,9 @@
+/* groovylint-disable-next-line MethodReturnTypeRequired */
+def sampleTest(){
+    echo "this is india"
+}
+def systemInfo(){
+    /* groovylint-disable-next-line NglParseError */
+    hostnamectl > /opt/sample.txt
+    
+}
